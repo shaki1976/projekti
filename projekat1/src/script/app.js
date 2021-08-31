8502f3cc1b5d00b51b6870a1b8a50e85efe8fe02
shaki1976 <amjsavic@gmail.com>
2021-08-31 23:05:10 +0200

    necu gotovo resenje , hocu sam utorak

---
+++ projekat1/src/script/app.js
@@ -0,0 +1,16 @@
+const budgetTitleMonth = document.querySelector(".budget__title--month");
+const budgetValue = document.querySelector(".budget__value");
+const budgetIncomeValue = document.querySelector(".budget__income--value");
+const budgetExpensesValue = document.querySelector(".budget__expenses--value");
+const budgetExpensesPercentage = document.querySelector(
+  ".budget__expenses--percentage"
+);
+
+const selectType = document.querySelector(".add__type");
+const addDescription = document.querySelector(".add__description");
+const addValue = document.querySelector(".add__value");
+
+const confirmButton = document.querySelector(".add__btn");
+
+const incomeList = document.querySelector(".income__list");
+const expensesList = document.querySelector(".expenses__list");
