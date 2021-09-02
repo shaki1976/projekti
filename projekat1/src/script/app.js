e442be3dbf662caa2de6a66923f49297d0edeb95
shaki1976 <amjsavic@gmail.com>
2021-09-02 15:10:11 +0200

    cetvrtak

--- projekat1/src/script/app.js
+++ projekat1/src/script/app.js
@@ -16,49 +16,67 @@ const incomeList = document.querySelector(".income__list");
 const expensesList = document.querySelector(".expenses__list");
 
 const brojaci = {
-  brojacRashoda = 0,
-  brojacPrihoda = 0,
+  brojacRashoda: 0,
+  brojacPrihoda: 0,
 }
 
 let podaci = {
-  allItems: { 
-  exp:[],
-  inc:[]
+  allItems: {
+    exp: [],
+    inc: []
   },
-  totals:{
-      exp:0,
-      inc:0
+  totals: {
+    exp: 0,
+    inc: 0
   },
   budget: 0,
   procenat: -1
 };
 
+function isValid() {
+  let isValid = true;
+  if(Number(addValue.value) < 0 || Number(addValue.value) == NaN) isValid = false;
+//  if(selectType.value != 'exp' || selectType.value != 'inc' ) isValid = false;
+ if(addDescription.value.trim() == "") isValid = false;
 
+return isValid;
+}
 
 class Item {
   constructor(id, description, value) {
     this.id = id;
-    this.value = value;
+    this.value = Number(value);
     this.description = description;
   }
-  addItem(tip, description, value){
+ static addItem(tip, description, value) {
+    if(!isValid()) return;
+
     let item;
     let id;
-    if(tip == 'inc') {
-      id = brojacPrihoda++;
-      item = new Income(id,description, value);
-prihodi++;
-    }else {
-      id = brojacRashoda++;
-      item = new Expense(id,description, value);
-      rashodi++;
+    if (tip == 'inc') {
+      id = brojaci.brojacPrihoda++;
+      item = new Income(id, description, value);
+      podaci.allItems.inc.push(item);
+    } else {
+      id = brojaci.brojacRashoda++;
+      item = new Expense(id, description, value);
+      podaci.allItems.exp.push(item);
     }
+    console.log(selectType.value)
+    console.log(isValid());
+    console.log(podaci);
     return item;
+  }
+
+  removeItem(id, tip){
+    let index = podaci.allItems[tip].findIndex((item) => item.id == id);
+    podaci.allItems[tip].splice(index,1);
   }
 }
 
 class Income extends Item {
-  super(id, description, value);
+  constructor(id, description, value){super(id, description, value)}
+  
 }
 
 class Expense extends Item {
@@ -68,3 +86,8 @@ class Expense extends Item {
   }
 }
 
+function addingItemHandler(){
+ Item.addItem(selectType.value, addDescription.value, addValue.value)
+}
+
+confirmButton.addEventListener('click', addingItemHandler )
